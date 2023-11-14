a2c10df136d9ce1467c35cbfbb7045379167dc3c
Raffi Sanna <rvs314@protonmail.com>
2023-11-14 09:57:39 -0500

    Split `test-check` into `test` and `check`

--- test-check.scm
+++ test-check.scm
@@ -1,18 +1,38 @@
+
+(define-syntax test
+  (syntax-rules ()
+    [(test title body ...)
+     (begin
+       (printf "Test ~s " title)
+       (guard (exn
+               [(error? exn)
+                (printf "FAILED: ~a~%" (condition-message exn))
+                #f])
+         body ...
+         (printf "passed~%")))]))
+
+(define-syntax check
+  (syntax-rules ()
+    [(check v)
+     (unless v
+       (error 'check
+              (format "Expected ~s to be truthy, but it was not" 'v)))]
+    [(check a b)
+     (let ([t a]
+           [v b])
+       (unless (equal? t v)
+         (error 'check
+                (format "expected: ~a, but computed: ~a" v t))))]))
+
 (define-syntax test-check
   (syntax-rules ()
     ((_ title tested-expression expected-result)
-     (begin
-       (printf "Testing ~a\n" title)
-       (let* ((expected expected-result)
-              (produced tested-expression))
-         (or (equal? expected produced)
-             (errorf 'test-check
-               "Failed: ~a~%Expected: ~a~%Computed: ~a~%"
-               'tested-expression expected produced)))))))
+     (test title
+       (check tested-expression expected-result)))))
 
 (define-syntax test-disable
   (syntax-rules ()
-    ((_ title tested-expression expected-result)
+    ((_ title other-forms ...)
      (begin
-       (printf "Disable testing ~s\n" title)
+       (printf "Test ~s disabled~%" title)
        #t))))
